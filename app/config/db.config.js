8111eee5e72b2f4842b4c0c780c8c6b425a145f3
Gabriel Neves <gabriel.neves@zrp.com.br>
2022-06-22 01:04:21 -0300

    tutorial utilizado 'https://www.unetiq.com/blog-posts/using-multiple-databases-with-nodejs-and-sequelize'

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,32 @@
 module.exports = {
-  HOST: process.env.DB_HOST,
-  USER: process.env.DB_USER,
-  PASSWORD: process.env.DB_PASSWORD,
-  DB: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
+  /**Declaration of databases for my development environment**/
+  "development": {
+    "databases": {}
+  },
+  // Special environment only for Database1
+  "Database1": {
+    "database": process.env.RDS_DATABASE1, //you should always save these values in environment variables
+    "username": process.env.RDS_USERNAME1,  //only for testing purposes you can also define the values here
+    "password":  process.env.RDS_PASSWORD1,
+    "host": process.env.RDS_HOSTNAME1,
+    "port": process.env.RDS_PORT1,
+    "dialect": "postgres"  //here you need to define the dialect of your databse, in my case it is Postgres
+  },
+  // Special environment only for Database2
+  "Database2": {
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+  },
+    
+}
+
--- app/models/index.js
+++ app/models/index.js
@@ -1,25 +1,65 @@
-const dbConfig = require("../config/db.config.js");
-
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  port: dbConfig.port,
-  operatorsAliases: false,
-  
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
-});
+const env = process.env.NODE_ENV || 'development';
 
+//Load the configuration from the config.js
+const dbConfig = require("../config/db.config.js")[env];
+
+//Create an empty object which can store our databases
 const db = {};
 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+//Extract the database information into an array
+const databases = Object.keys(dbConfig.databases);
 
-db.events = require("./event.model.js")(sequelize, Sequelize);
+//Loop over the array and create a new Sequelize instance for every database from config.js
+for(let i = 0; i < databases.length; ++i) {
+  let database = databases[i];
+  let dbPath = dbConfig.databases[database];
+  //Store the database connection in our db object
+  db[database] = new Sequelize(dbPath.DB, dbPath.USER, dbPath.PASSWORD, {
+    host: dbPath.HOST,
+    dialect: dbPath.dialect,
+    port: dbPath.port,
+    operatorsAliases: false,
+    
+    pool: {
+      max: dbPath.pool.max,
+      min: dbPath.pool.min,
+      acquire: dbPath.pool.acquire,
+      idle: dbPath.pool.idle
+    }
+  });  
+}
+
+/**Add the Database Models**/
+//Add models from database1 folder
+fs
+  .readdirSync(__dirname + '/database1')
+  .filter(file =>
+      (file.indexOf('.') !== 0) &&
+      (file !== basename) &&
+      (file.slice(-3) === '.js'))
+  .forEach(file => {
+      const model = db.Database1.import(path.join(__dirname + '/database1', file));
+      db[model.name] = model;
+  });
+
+// Add models from database2 folder
+
+fs
+  .readdirSync(__dirname + '/database2')
+  .filter(file =>
+      (file.indexOf('.') !== 0) &&
+      (file !== basename) &&
+      (file.slice(-3) === '.js'))
+  .forEach(file => {
+      const model = db.Database2.import(path.join(__dirname + '/database2', file));
+      db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+      db[modelName].associate(db);
+  }
+});
 
 module.exports = db;
